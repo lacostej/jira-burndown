c7e5f0a4a29483f67a5565352207e20400b9962f
Jukka Lindstrom <jslindst@laughingpanda.org>
2006-01-22 21:23:10 +0000

    fixes the bug of showing all projects from jira instead of showing those for which there are rights. a fix is still needed for the portlet itself to prevent viewing illegal projects
    git-svn-id: http://svn.laughingpanda.org/svn/agile-plugins/trunk@23 2c34f819-adf7-0310-b77d-b7e162b7d64c

--- src/main/java/com/laughingpanda/jira/AllVersionValuesGenerator.java
+++ src/main/java/com/laughingpanda/jira/AllVersionValuesGenerator.java
@@ -14,9 +14,13 @@ import org.ofbiz.core.entity.GenericValue;
 
 import com.atlassian.configurable.ValuesGenerator;
 import com.atlassian.jira.ComponentManager;
+import com.atlassian.jira.ManagerFactory;
 import com.atlassian.jira.project.ProjectManager;
 import com.atlassian.jira.project.version.Version;
 import com.atlassian.jira.project.version.VersionManager;
+import com.atlassian.jira.security.PermissionManager;
+import com.atlassian.jira.security.Permissions;
+import com.opensymphony.user.User;
 
 /**
  * Returns all the different possible versions sorted under projects. Projects
@@ -29,25 +33,26 @@ public class AllVersionValuesGenerator implements ValuesGenerator {
     private final Category log = Category.getInstance(AllVersionValuesGenerator.class);
 
     private VersionManager versionManager;
-    private ProjectManager projectManager;
+    private PermissionManager permissionManager;
 
     public AllVersionValuesGenerator() {
         setVersionManager(ComponentManager.getInstance().getVersionManager());
-        setProjectManager(ComponentManager.getInstance().getProjectManager());
+        setPermissionManager(ManagerFactory.getPermissionManager());
     }
 
     public void setVersionManager(VersionManager versionManager) {
         this.versionManager = versionManager;
     }
 
-    public void setProjectManager(ProjectManager projectManager) {
-        this.projectManager = projectManager;
+    public void setPermissionManager(PermissionManager permissionManager) {
+        this.permissionManager = permissionManager;
     }
 
     public Map getValues(Map params) {
+        User u = (User) params.get("User");                
         Map<Long,String> selection = new SequencedHashMap();
         try {
-            Collection<GenericValue> projects = projectManager.getProjects();
+            Collection<GenericValue> projects = permissionManager.getProjects(Permissions.BROWSE, u);
             for (GenericValue project : projects) {
                 selection.put(new Long(-project.getLong("id").longValue()), project.getString("name"));
                 Collection<Version> versions = versionManager.getVersions(project);
